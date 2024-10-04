7a1b36ac7a9b0a6ba202d4417cfbf180eced423b
Roosych <ruslankandiba88@gmail.com>
2024-10-04 14:07:05 +0400

    ticket status, actions az trans

---
+++ lang/az/tickets.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'statuses' => [
+        'opened' => 'Açıq',
+        'in_progress' => 'İcra olunur',
+        'done' => 'İcra olundu',
+        'completed' => 'Tamamlandı',
+        'canceled' => 'Ləğv olunub',
+    ],
+    'actions' => [
+        'update_status' => 'Status dəyişdirildi:',
+        'commented' => 'Yeni şərh',
+        'assign_user' => 'İcraçı əlavə edildi',
+        'create_child' => 'Alt tiket yaradıldı',
+    ],
+];
